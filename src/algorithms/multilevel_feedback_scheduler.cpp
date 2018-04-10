304577ec70c62202b1ea33ee3339dd81540239d1
Katrina Steinman <ksteinma@bb136-10.mines.edu>
2018-04-09 19:44:43 -0600

    tried working more on MLFQ

--- src/algorithms/multilevel_feedback_scheduler.cpp
+++ src/algorithms/multilevel_feedback_scheduler.cpp
@@ -1,5 +1,6 @@
 #include "algorithms/multilevel_feedback_scheduler.h"
 #include "types/process.h"
+#include "types/thread.h"
 
 using namespace std;
 
@@ -11,6 +12,10 @@ SchedulingDecision* MultilevelFeedbackScheduler::get_next_thread(
 //	robins[i] = robin;
   //}
 
+//(event->thread){
+//f(
+//
+
   SchedulingDecision* dec = new SchedulingDecision();
 
 //  for(int i = 0; i < NUM_QUEUES; i++){
@@ -20,21 +25,47 @@ SchedulingDecision* MultilevelFeedbackScheduler::get_next_thread(
 //	}
  // }
 if(event){
-  if(q1.size() > 0){
+  if((q1.size() > 0)){ //&& (q1.front()->current_state == Thread::State::EXIT)){
 	dec->thread = q1.front();
+	
+	//	q2.push(q1.front());
+	//}
 	q1.pop();
+	/*if((event->thread->previous_state == Thread::State::RUNNING) && (event->thread->current_state != Thread::State::EXIT)){
+		q2.push(event->thread);
+	}*/
   }else if(q2.size() > 0){
 	dec->thread = q2.front();
+//	q3.push(q2.front());
 	q2.pop();
+	/*if((event->thread->previous_state == Thread::State::RUNNING) && (event->thread->current_state != Thread::State::EXIT)){
+		q3.push(event->thread);
+	}*/
+
   }else if(q3.size() > 0){
 	dec->thread = q3.front();
+//	q4.push(q3.front());
 	q3.pop();
+	/*if((event->thread->previous_state == Thread::State::RUNNING) && (event->thread->current_state != Thread::State::EXIT)){
+		q4.push(event->thread);
+	}*/
+
   }else if(q4.size() > 0){
 	dec->thread = q4.front();
+//	q5.push(q4.front());
 	q4.pop();
+	/*if((event->thread->previous_state == Thread::State::RUNNING) && (event->thread->current_state != Thread::State::EXIT)){
+		q5.push(event->thread);
+	}*/
+
   }else if(q5.size() > 0){
 	dec->thread = q5.front();
+//	q6.push(q5.front());
 	q5.pop();
+	/*if((event->thread->previous_state == Thread::State::RUNNING) && (event->thread->current_state != Thread::State::EXIT)){
+		q6.push(event->thread);
+	}*/
+
   }else if(q6.size() > 0){
 	dec->thread = q6.front();
 	q6.pop();
@@ -68,8 +99,6 @@ if(thread){
 
  //favor threads that are preempted less
  }
-  
-
 }
 
 
